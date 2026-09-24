c31e712d9a701300ed15db1797cbecdf3e49bb72
davyorellkoukponou-coder <davyorellkoukponou@gmail.com>
2026-09-24 18:45:55 +0100

    nouveau lien de paiement

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Collez votre lien de paiement Chariow entre les guillemets lorsque vous l'aurez reçu. */
 window.ARTISAN360 = {
-  paymentUrl: "https://davdigit.com/artisan-360-le-kit-digital-complet/checkout",
+  paymentUrl: "https://davdigital.mychariow.shop/artisan-360-le-kit-digital-complet/checkout",
   price: "3 999 FCFA"
 };
